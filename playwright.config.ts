e5a3297bd239f0475e427df685d9eb507694a35c
MariamMilad <104933286+mariamm-milad@users.noreply.github.com>
2026-04-14 12:15:02 +0200

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -28,7 +28,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    baseURL: 'http://robotesta.ai',
+    baseURL: 'http://robotesta.ai/login',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -39,7 +39,7 @@ export default defineConfig({
     {
       name: 'chromium',
       use: {
-        headless: false,
+        headless: true,
          ...devices['Desktop Chrome'] },
     },
 
